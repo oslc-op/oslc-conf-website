918cc5bafacbdf8e4430a5c3565e19ed451c9bab
Gabriel Fernandes de Araujo Costa <gabrielfdac@gmail.com>
2018-06-21 23:08:52 -0300

    Adds dynamic modals, content and style tweaks

--- themes/oslc-conference/static/js/main.js
+++ themes/oslc-conference/static/js/main.js
@@ -19,6 +19,43 @@ function scrollToggle () {
 }
 //END Helper
 
+function openInfo (source) {
+	presenter = $(source).attr('data-bio');
+	$('#infoCard img').attr('src', bio[presenter].picturePath);
+	$('#infoCard .item-title').html(bio[presenter].name);
+	$('#infoCard .titles').html(bio[presenter].title);
+	$('#infoCard .info--bio').children().remove();
+	for(i = 0;i < bio[presenter].bio.length;i++) {
+		$('#infoCard .info--bio').append('<p>'+bio[presenter].bio[i]+'</p>');
+	}
+	$('#infoCard .social-icons').children().remove();
+	for(i = 0;i < bio[presenter].contact.length;i++) {
+		$('#infoCard .social-icons').append($('<a href="'+ bio[presenter].contact[i].link +'" target="_blank">'+ bio[presenter].contact[i].icon +'</a>'))
+	}
+	toggleElement('', '#infoCard');
+}
+
+
+function toggleElement(target, selector) {
+	if($(target).hasClass('active')) {
+		$(selector).add($(target)).removeClass('active');
+		$('.overlay').remove();
+	}
+	else if($(selector).hasClass('active')){
+		$(selector).removeClass('active');
+		$('[data-select="'+selector+'"]').removeClass('active');
+		$('.overlay').remove();
+	} else{
+		$(selector).add($(target)).addClass('active');
+		if(!$(target).hasClass('no-overlay')) {
+			$('<div class="overlay"></div>').click(function() {
+				$(selector).add($(target)).removeClass('active');
+				$(this).remove();
+			}).appendTo($(document.body));
+		}
+	}
+}
+
 $(document).ready(function () {
 	//Initialization
 
@@ -42,23 +79,7 @@ $(document).ready(function () {
 		var target = $(this);
 		var selector = $(this).attr('data-select');
 
-		if($(target).hasClass('active')) {
-			$(selector).add($(target)).removeClass('active');
-			$('.overlay').remove();
-		}
-		else if($(selector).hasClass('active')){
-			$(selector).removeClass('active');
-			$('[data-select="'+selector+'"]').removeClass('active');
-			$('.overlay').remove();
-		} else{
-			$(selector).add($(target)).addClass('active');
-			if(!$(target).hasClass('no-overlay')) {
-				$('<div class="overlay"></div>').click(function() {
-			    	$(selector).add($(target)).removeClass('active');
-			    	$(this).remove();
-			  	}).appendTo($(document.body));
-		  	}
-		}
+		toggleElement(target, selector);
 	});
 	//END Event Handlers
 
