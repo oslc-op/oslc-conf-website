639b3a35aec88854abc1e7e604a948b8fedb3c9d
isccarrasco <mario.carrasco@gmail.com>
2022-11-15 10:26:59 -0600

    Updating profile pic
    Signed-off-by: isccarrasco <mario.carrasco@gmail.com>

--- themes/oslc-conference/static/js/bio-21-5.js
+++ themes/oslc-conference/static/js/bio-21-5.js
@@ -949,7 +949,7 @@ let bio = {
   jimT: {
     'name': 'Jim Tung',
     'title': 'MathWorks Fellow',
-    'picturePath':  '/images/speakers/jimtung.jpg',
+    'picturePath':  '/images/speakers/improved_tungjim.jpg',
     'bio': [
       "Jim Tung is a 30-year veteran of MathWorks, focusing on business and technology strategy, and working with key customers and partners. Jim previously held the positions of vice president of marketing and vice president of business development. Prior to joining MathWorks, Jim held management positions at Lotus Development and Keithley DAS, a pioneering manufacturer of PC-based data acquisition systems."
     ],
